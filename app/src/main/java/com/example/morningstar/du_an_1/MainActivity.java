197284c0f6d8d6c33efea2bab4740a0bd2ca3fe2
Morningstar <nghia181998@gmail.com>
2018-11-09 12:32:12 +0700

    Chờ

--- app/src/main/java/com/example/morningstar/du_an_1/MainActivity.java
+++ app/src/main/java/com/example/morningstar/du_an_1/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.morningstar.du_an_1;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CountDownTimer countDownTimer = new CountDownTimer(3000,3000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                finish();
+                startActivity(new Intent(getApplicationContext(),Home.class));
+            }
+        }.start();
     }
 }
